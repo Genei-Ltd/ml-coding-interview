04b17247c78f952254fd3eac94e06e4f03fd53c4
Ubuntu <ubuntu@ip-172-31-82-31.ec2.internal>
2022-08-02 19:30:09 +0000

    basic prompts for task 1

--- train.py
+++ train.py
@@ -1,52 +1,45 @@
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 import torch
-import math
 from model import Config
 from model import GPT
-from torch.optim import AdamW
-
-# HYPERPARAMS
-BLOCK_SIZE = 1024
-LR=5e-4
-EPOCHS=3
-BATCH_SIZE=2
-NGPUS = 4
 
 # Tokenizer
 class CharTokenizer:
     def __init__(self, text):
-        vocab = set(text)
-        self.vocab_size = len(vocab)
-        self.stoi = {x:i for i,x in enumerate(vocab)}
-        self.itos = {v:k for k,v in self.stoi.items()}
+        """
+        Take in the full pretraining text, compute vocab size.
+        """
 
     def encode(self, xs):
-        return torch.tensor([self.stoi[x] for x in xs], dtype=torch.long)
+        """
+        Input sequence of chars.
+        Output torch LONG tensor of indicies.
+        """
 
     def decode(self, xs):
-        return ''.join([self.itos[x] for x in xs])
+        """
+        Input torch long tensor of indicies.
+        Output character string.
+        """
 
 # Dataset
 class CharDataset(Dataset):
     def __init__(self, fpath, block_size):
-        self.fpath = fpath
-        self.block_size = block_size
-
-        with open(fpath, 'r') as f:
-            self.text = f.read()
-
-        self.tokenizer = CharTokenizer(self.text)
+        """
+        Read in a file of pretraining text.
+        Create a tokenizer from corpus.
+        """
 
     def __getitem__(self, idx):
-        start_idx = idx * self.block_size
-        end_idx = start_idx + self.block_size + 1
-        chunk = self.text[start_idx:end_idx]
-        idxs = self.tokenizer.encode(chunk)
-        return {
-            'x': idxs[:-1],
-            'y': idxs[1:]
-        }
+        """
+        For each idx return a block_size block of tensors.
+        Specificially return dict with keys.
+        - x: input indicies (torch long tensor)
+        - y: next token target indicies (torch long tensor)
+        """
 
     def __len__(self):
-        return (len(self.text)-1) // self.block_size
+        """
+        Length of dataset.
+        """
 
